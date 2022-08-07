924022182d241f388e20e363c7b5f53ad999d389
Taishi-Ito <taishi.ito1026@gmail.com>
2022-08-08 08:51:15 +0900

    Add work group dependent destroy

--- app/models/api/v1/user.rb
+++ app/models/api/v1/user.rb
@@ -1,5 +1,5 @@
 class Api::V1::User < ApplicationRecord
-  has_many :api_v1_work_groups
+  has_many :api_v1_work_groups, dependent: :destroy
   validates :uid, presence: true
   validates :name, presence: true
   validates :locale, presence: true
