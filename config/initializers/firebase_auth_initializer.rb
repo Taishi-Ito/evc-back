4d2109334f92b6d2f8ed83ecc6312b370dbfa9d1
Taishi-Ito <taishi.ito1026@gmail.com>
2022-08-08 21:31:51 +0900

    fix config redis

--- config/initializers/firebase_auth_initializer.rb
+++ config/initializers/firebase_auth_initializer.rb
@@ -1,4 +1,8 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new
+  if Rails.env.production?
+    config.redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+  else
+    config.redis = Redis.new
+  end
   config.project_ids = ['evc-project-4b593']
 end
