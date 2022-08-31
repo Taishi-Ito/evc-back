dfca82cf3e0a0d79404507908f2f60f2f6da9733
Taishi-Ito <taishi.ito1026@gmail.com>
2022-08-31 19:27:44 +0900

    Create project#index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,10 @@
 class ProjectsController < ApplicationController
+  def index
+    work_group_project_lists = Project.get_wg_pj_lists current_user.id
+    if work_group_project_lists
+      render json: {work_group_project_lists: work_group_project_lists}, status: 200
+    else
+      render json: {message: "ワークグループを取得できませんでした。"}, status: 404
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,21 @@
 class Project < ApplicationRecord
   belongs_to :work_group
   validates :title, presence: true
+
+  def self.get_wg_pj_lists user_id
+    work_groups = WorkGroup.where(user_id: user_id)
+    wg_pj_lists = []
+    work_groups.each do |work_group|
+      wg_pj_list = {}
+      projects = Project.where(work_group_id: work_group.id)
+      wg_pj_list["workGroupTitle"] = {"title": work_group.title, "id": work_group.id}
+      wg_pj_list["projectTitles"] = []
+      projects.each do |project|
+        project_set = {"title": project.title, "id": project.id}
+        wg_pj_list["projectTitles"].push(project_set)
+      end
+      wg_pj_lists.push(wg_pj_list)
+    end
+    wg_pj_lists
+  end
 end
