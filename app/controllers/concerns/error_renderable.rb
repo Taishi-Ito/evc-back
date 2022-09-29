50abac89100851ddfcbf20626ce21417e1ac3b0e
Taishi-Ito <taishi.ito1026@gmail.com>
2022-09-29 09:16:40 +0900

    Add range error response

--- app/controllers/concerns/error_renderable.rb
+++ app/controllers/concerns/error_renderable.rb
@@ -14,5 +14,8 @@ module ErrorRenderable
     rescue_from ActiveRecord::RecordInvalid do |e|
       render json: {message: "入力値を確認してください。<br> #{e}"}, status: 422
     end
+    rescue_from ActiveRecord::RangeError do |e|
+      render json: {message: "年は最長4桁の整数、単位が%のものは最長3桁+小数点3桁の数字、それ以外は最長16桁+小数点3桁の数字の範囲で入力してください。"}, status: 400
+    end
   end
 end
