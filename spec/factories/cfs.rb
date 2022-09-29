6830ea9b74fad56d47c4a25ba74cc34b4f11d3df
Taishi-Ito <taishi.ito1026@gmail.com>
2022-09-30 08:36:38 +0900

    Create cf model test

---
+++ spec/factories/cfs.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :cf do
+    id { 1 }
+    project_id { 1 }
+    title { "テスト" }
+    unit { "yen" }
+    fixed { 1 }
+    created_at { "2022-09-01" }
+    updated_at { "2022-09-01" }
+  end
+end
---
+++ spec/models/cf_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Cf, type: :model do
+  before(:each) {@pj = FactoryBot.build(:project)}
+  subject { @pj }
+
+  it "project,unit,fixedがあれば有効である。" do
+    @pl = Pl.new(id: 1, project: @pj, title: "テスト", unit: "yen", fixed: 1, created_by: "testuid", edited_by: "testuid", created_at: "2022-09-01", updated_at: "2022-09-01")
+    expect(@pl).to be_valid
+  end
+
+  it "projectがない場合、無効である" do
+    @pl = Pl.new(id: 1, project: nil, title: "テスト", unit: "yen", fixed: 1, created_by: "testuid", edited_by: "testuid", created_at: "2022-09-01", updated_at: "2022-09-01")
+    expect(@pl).to be_invalid
+  end
+
+  it "unitがない場合、無効である" do
+    @pl = Pl.new(id: 1, project: @pj, title: "テスト", unit: nil, fixed: 1, created_by: "testuid", edited_by: "testuid", created_at: "2022-09-01", updated_at: "2022-09-01")
+    expect(@pl).to be_invalid
+  end
+
+  it "fixedがない場合、無効である" do
+    @pl = Pl.new(id: 1, project: @pj, title: "テスト", unit: "yen", fixed: nil, created_by: "testuid", edited_by: "testuid", created_at: "2022-09-01", updated_at: "2022-09-01")
+    expect(@pl).to be_invalid
+  end
+end
