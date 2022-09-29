6ba995516cb3679d6c6d3bf67712e6b24b99f83f
Taishi-Ito <taishi.ito1026@gmail.com>
2022-09-29 18:55:49 +0900

    Create capitalinvestment model test

--- spec/factories/capital_investments.rb
+++ spec/factories/capital_investments.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
-  factory :project do
+  factory :capital_investment do
     id { 1 }
-    work_group_id { 1 }
+    project_id { 1 }
     title { "テスト" }
+    unit { "yen" }
+    fixed { 1 }
     created_by { "testuid" }
+    edited_by { "testuid" }
     created_at { "2022-09-01" }
     updated_at { "2022-09-01" }
   end
